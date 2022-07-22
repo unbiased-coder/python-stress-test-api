be4aec57853df360b40107d426a4b1dfa86d54da
Unbiased Coder <info@unbiased-coder.com>
2022-07-22 23:06:26 +0300

    initial commit for python load testing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# python-stress-test-api
 Python Load and Performance Test REST API
+
+We talk about everything you need to perform your stress testing with Python
+
+You can find a full tutorial in the link below:
---
+++ load-test-api.py
@@ -0,0 +1,26 @@
+import requests
+import datetime
+import concurrent.futures
+
+HOST = 'https://jsonplaceholder.typicode.com'
+API_PATH = '/todos/'
+ENDPOINT = HOST + API_PATH
+MAX_THREADS = 4
+CONCURRENT_THREADS = 3
+
+def send_api_request():
+    print ('Sending API request: ', ENDPOINT)
+    r = requests.get(ENDPOINT)
+    print ('Received: ', r.status_code, r.text)
+
+start_time = datetime.datetime.now()
+print ('Starting:', start_time)
+
+
+with concurrent.futures.ThreadPoolExecutor(MAX_THREADS) as executor:
+    futures = [ executor.submit(send_api_request) for x in range (CONCURRENT_THREADS) ]
+
+import time
+time.sleep(5)
+end_time = datetime.datetime.now()
+print ('Finished start time:', start_time, 'duration: ', end_time-start_time)
